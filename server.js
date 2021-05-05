97e93b61d8a6a8b27e95baad6b0e5cdbe9cde898
Maique Rosa <maique.rosa@bitz.com.br>
2021-05-05 18:45:50 -0300

    chore: setando porta através do enviroment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
 
-app.listen(3333, () => {
-  console.log("Servidor em execução na port 3333");
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  console.log(`Servidor em execução na port ${port}`);
 });
